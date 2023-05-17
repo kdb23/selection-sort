ee716699151dc6b0691ae8b4231df43876e6da66
Kimberly Benton <kimberly.d.benton@gmail.com>
2023-05-17 12:33:48 -0400

    test passing

--- javascript/selection_sort.js
+++ javascript/selection_sort.js
@@ -1,9 +1,29 @@
 function selectionSort(arr) {
-  // type your code here
+  const length = arr.length;
+
+  for (let i = 0; i < length - 1; i++) {
+    let minIndex = i;
+
+    // Find the index of the minimum element in the unsorted part of the array
+    for (let j = i + 1; j < length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    // Swap the minimum element with the first unsorted element
+    if (minIndex !== i) {
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
+  }
+
+  return arr;
 }
 
 if (require.main === module) {
-  // add your own tests in here
+
+  console.log(selectionSort([1,4,7,20]))
+
   console.log("Expecting: [-1, 2, 3, 5]");
   console.log("=>", selectionSort([3, -1, 5, 2]));
 
